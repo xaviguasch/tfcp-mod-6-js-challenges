ef52f860cb34a8242b283eecd0220cb9f2c422e2
xaviguasch <xaviguasch@gmail.com>
2020-08-07 20:33:42 +0200

    added solution to case insensitive palindrome

--- 02-case-insensitive-palindrome.js
+++ 02-case-insensitive-palindrome.js
@@ -1,5 +1,17 @@
 function caseInsensitivePalindrome(str) {
   //  write code here.
+
+  const strLowerArr = str.toLowerCase().split('')
+
+  const newArr = [...strLowerArr]
+
+  const reversedArr = newArr.reverse()
+
+  if (reversedArr === strLowerArr) {
+    return true
+  } else {
+    return false
+  }
 }
 
-console.log(caseInsensitivePalindrome('AaBaa'))
+console.log(caseInsensitivePalindrome('abac'))
