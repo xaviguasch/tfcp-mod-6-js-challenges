aec8d7d16528dcfe432fb64bd7650040f74aa40a
xaviguasch <xaviguasch@gmail.com>
2020-08-07 21:44:40 +0200

    added solution to First Digit

--- 05-first-digit.js
+++ 05-first-digit.js
@@ -1,5 +1,14 @@
 function firstDigit(str) {
   //  write code here.
+  const chars = str.split('')
+
+  const strNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+  for (const char of chars) {
+    if (strNums.includes(char)) {
+      return char
+    }
+  }
 }
 
-console.log(firstDigit('var1__Int'))
+console.log(firstDigit('var__Int22....14'))
