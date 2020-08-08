7ed5e1d34bc37f795f3d6515550fe07b88eeba21
xaviguasch <xaviguasch@gmail.com>
2020-08-08 10:58:19 +0200

    added solution for Largest Number

--- 06-largest-number.js
+++ 06-largest-number.js
@@ -1,5 +1,14 @@
 function largestNumber(num) {
   //  write code here.
+  // let placeholder = ''
+  // for (let i = 0; i < num; i++) {
+  //   placeholder = placeholder.concat('9')
+  // }
+  // return parseInt(placeholder)
+
+  const placeholder = '9'.repeat(num)
+
+  return parseInt(placeholder)
 }
 
-console.log(largestNumber(2))
+console.log(largestNumber(4))
