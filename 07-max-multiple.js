c5bf1ffbf99cc8c7e88f07aa51d4efab156fce66
xaviguasch <xaviguasch@gmail.com>
2020-08-08 11:15:59 +0200

    added solution for max multiple

--- 07-max-multiple.js
+++ 07-max-multiple.js
@@ -1,5 +1,10 @@
 function maxMultiple(divisor, bound) {
   //  write code here.
+  const remainder = bound % divisor
+
+  const result = bound - remainder
+
+  return result
 }
 
 console.log(maxMultiple(3, 10))
